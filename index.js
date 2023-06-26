63c5befba44afcf446b38d36214d6b1bd3d33415
Rokeya Sarker <133252712+rokeyasarker49@users.noreply.github.com>
2023-06-27 04:04:34 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+// function startFan(){
+        // console.log("Press the switch of the fan")
+// }
+
+// startFan()
+// console.log("today is a very hot day")
+// console.log("today is a very hot day")
+// console.log("today is a very hot day")
+// console.log("today is a very hot day")
+// console.log("today is a very hot day")
+// console.log("today is a very hot day")
+// /startFan()
+
+// function bringTomato(taka){
+        // console.log(taka);
+        // var priceOfTomato = 20;
+        // var qtyOfTomato = taka / priceOfTomato;
+        // console.log(qtyOfTomato);
+        // return qtyOfTomato;
+// }
+
+// console.log(bringTomato(200));
+var totalTomato = bringTomato(200);
+//console.log(totalTomato);
+
+function sumOfTwoNumbers(num1, num2){
+        // console.log(num1, num2);
+        var total = num1 + num2;
+        // console.log(total)
+        // return total;
+}
+
+var result = sumOfTwoNumbers(1,2)
+// console.log(result);
+
+function multiplyOfTwoNumbers(num1, num2, num3){
+        // console.log(num1, num2, num3);
+        var multiply = num1 + num2 + num3;
+        // console.log(multiply);
+        // return multiply;
+}
+
+var result = multiplyOfTwoNumbers(20,30,40);
+// console.log(result);
+var divide = result/2;
+// console.log(divide);
+
+var about= {
+        name: "Rokeya" ,
+        gender: "female"
+}
+// console.log(about);
+
+var myPC= {
+        brand: "hp",
+        generation: "core i5 8th gen"
+        price: 200
+}
+
+// console.log(myPC);
+// myPC.price = 20000;
+myPC["price"] = 20000;
+console.log(myPC);
